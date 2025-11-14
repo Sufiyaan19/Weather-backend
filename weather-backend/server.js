366aab9a3f4fe4979d48cefb036c3ad5d055250e
shaheen240 <sufiyaannayakodi@gmail.com>
2025-11-14 20:36:30 +0530

    Update server.js to use environment variable for PORT

--- weather-backend/server.js
+++ weather-backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Enable CORS for all routes
 app.use(cors({
